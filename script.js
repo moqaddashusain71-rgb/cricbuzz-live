366a44bbd7bbd84618305e0b4a64955a91746c08
moqaddashusain71-rgb <moqaddashusain71@gmail.com>
2026-03-10 23:54:21 +0530

    Refactor fetch and score handling in loadMatches

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const API = "https://api.cricapi.com/v1/currentMatches?apikey=6b6d143f-b3b8-4a38-97c5-2ba6f5fbb7eb";
+const API="https://api.cricapi.com/v1/currentMatches?apikey=6b6d143f-b3b8-4a38-97c5-2ba6f5fbb7eb";
 
 async function loadMatches(){
 
-const res = await fetch(API);
-const data = await res.json();
+const res=await fetch(API);
+const data=await res.json();
 
-const matches = data.data;
+const matches=data.data;
 
-const container = document.getElementById("matches");
+const container=document.getElementById("matches");
 
 container.innerHTML="";
 
@@ -20,13 +20,9 @@ let scoreHTML="";
 
 if(match.score){
 
-match.score.forEach(s=>{
+match.score.forEach((s,i)=>{
 
-let team=s.inning
-.replace("Inning 1","")
-.replace("Inning 2","")
-.split(",")[0]
-.trim();
+let team=match.teams ? match.teams[i] : "Team";
 
 scoreHTML+=`
 <p class="score">
