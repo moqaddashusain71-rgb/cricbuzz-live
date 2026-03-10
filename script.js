9126508ddfcb26d9da94c2fbae926558954578b2
moqaddashusain71-rgb <moqaddashusain71@gmail.com>
2026-03-10 13:43:47 +0530

    Update API key and enhance match score handling
    Updated API key and improved score display logic.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "6b6d143f-b3b8-4a38-97c5-2ba6f5fbb7eb";
 
 async function loadMatches(){
 
@@ -13,6 +13,17 @@ let matchesHTML = "";
 
 data.data.forEach(match => {
 
+let score = "-";
+
+if(match.score && match.score.length > 0){
+
+score =
+match.score[0].r + "/" +
+match.score[0].w +
+" (" + match.score[0].o + " ov)";
+
+}
+
 matchesHTML += `
 
 <div class="match-card">
@@ -21,12 +32,12 @@ matchesHTML += `
 
 <div class="team">
 <span>${match.teamInfo[0].name}</span>
-<span>${match.score?.[0]?.r || "-"} / ${match.score?.[0]?.w || "-"}</span>
+<span>${score}</span>
 </div>
 
 <div class="team">
 <span>${match.teamInfo[1].name}</span>
-<span>-</span>
+<span></span>
 </div>
 
 <div class="status">
