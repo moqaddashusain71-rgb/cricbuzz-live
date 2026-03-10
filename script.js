31806983dba56dd102f02f79b85a48c99ae724ff
moqaddashusain71-rgb <moqaddashusain71@gmail.com>
2026-03-10 23:44:00 +0530

    Refactor code formatting and improve readability

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const API="https://api.cricapi.com/v1/currentMatches?apikey=6b6d143f-b3b8-4a38-97c5-2ba6f5fbb7eb";
+const API = "https://api.cricapi.com/v1/currentMatches?apikey=6b6d143f-b3b8-4a38-97c5-2ba6f5fbb7eb";
 
 async function loadMatches(){
 
-const res=await fetch(API);
-const data=await res.json();
+const res = await fetch(API);
+const data = await res.json();
 
-const matches=data.data;
+const matches = data.data;
 
-const container=document.getElementById("matches");
+const container = document.getElementById("matches");
 
 container.innerHTML="";
 
@@ -22,7 +22,11 @@ if(match.score){
 
 match.score.forEach(s=>{
 
-let team=s.inning.split(" ")[0];
+let team=s.inning
+.replace("Inning 1","")
+.replace("Inning 2","")
+.split(",")[0]
+.trim();
 
 scoreHTML+=`
 <p class="score">
